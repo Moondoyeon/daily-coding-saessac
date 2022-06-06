07b8d26d4856222791a9cc66c9df7219da70defc
moondoyeon <mmmdo21@gmail.com>
2022-06-07 06:19:53 +0900

    jeju-007

--- jeju-coding/007.js
+++ jeju-coding/007.js
@@ -1,4 +1,4 @@
-// 2,3
+// 2,3,5
 
 let let = 1; 
 // => 오류 let is disallowed as a lexically bound name 
@@ -9,4 +9,5 @@ let &age = 1;
 // =>오류 Invalid left-hand side in assignment
 // 부적절한 위치에서 할당 행위를 하려고 할 때 발생하는 에러
 
-
+let 1age = 1;
+// => 오류 Invalid or unexpected token
