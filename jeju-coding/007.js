c4ce4130a7a41d4977b6239bc62ddcb20707cbb8
moondoyeon <mmmdo21@gmail.com>
2022-06-07 06:05:23 +0900

    007.js

---
+++ jeju-coding/007.js
@@ -0,0 +1,12 @@
+// 2,3
+
+let let = 1; 
+// => 오류 let is disallowed as a lexically bound name 
+var let = 1;
+// => 오류가 뜨지 않음.
+
+let &age = 1;
+// =>오류 Invalid left-hand side in assignment
+// 부적절한 위치에서 할당 행위를 하려고 할 때 발생하는 에러
+
+
